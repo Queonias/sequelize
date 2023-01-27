4783126907024324ff410eb2f5941ccf9381897b
queones <queones@gmail.com>
2023-01-27 14:11:53 -0300

    [refactor] config.js - [feat] index.js - [refactor] models/index.js

---
+++ src/config/config.js
@@ -0,0 +1,39 @@
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+
+// src/config/config.js
+
+const config = {
+  username: root,
+  password: senha_mysql,
+  database: orm_example,
+  host: localhost,
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
