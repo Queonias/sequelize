5d710928ad452ee7b71a5b0129fb8917bfa75fec
queones <queones@gmail.com>
2023-01-27 19:47:35 -0300

    feat: exercicio bônus concluido com sucesso

--- src/migrations/20230127221826-add-publisher-to-books.js
+++ src/migrations/20230127221826-add-publisher-to-books.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Books', 'publisher', { 
+    await queryInterface.addColumn('Books', 'publisher', { 
       type: Sequelize.STRING,
       allowNull: true
        });
