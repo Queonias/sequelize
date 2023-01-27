0f2422016eb7eaa320070c6cec8c48dc2430d721
queones <queones@gmail.com>
2023-01-27 15:15:27 -0300

    [feat] tabela users criada

--- src/migrations/20230127171820-create-books.js
+++ src/migrations/20230127171820-create-books.js
@@ -1,7 +1,37 @@
 'use strict';
 
+const { DataTypes } = require("sequelize/types");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      }
+    });
     /**
      * Add altering commands here.
      *
@@ -10,7 +40,8 @@ module.exports = {
      */
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('Books');
     /**
      * Add reverting commands here.
      *
