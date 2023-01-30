689b18536317b6cf68e0bd8187baf919816730e6
queones <queones@gmail.com>
2023-01-30 10:34:15 -0300

    Eager Loading

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -2,7 +2,7 @@ const { Address, Employee } = require('../models');
 
 const getAll = async () => {
     const users = await Employee.findAll({
-        include: [{ model: Address, as: 'addresses' }],
+        include: { model: Address, as: 'addresses' },
     });
 
     return users;
@@ -11,7 +11,7 @@ const getAll = async () => {
 const getById = async (id) => {
     const employee = await Employee.findOne({
         where: { id },
-        include: [{ model: Address, as: 'addresses' }],
+        include: [{ model: Address, as: 'addresses', attributes: { exclude: ['id', 'employeeId'] } }],
       });
     return employee;
   }
