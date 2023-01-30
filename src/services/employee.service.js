b2a7121ee63b54437c0156eee1fe816f589539ca
queones <queones@gmail.com>
2023-01-30 10:30:13 -0300

    include com todos os atrubutos

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -2,12 +2,21 @@ const { Address, Employee } = require('../models');
 
 const getAll = async () => {
     const users = await Employee.findAll({
-        includes: { model: Address, as: 'addresses' },
+        include: [{ model: Address, as: 'addresses' }],
     });
 
     return users;
 };
 
+const getById = async (id) => {
+    const employee = await Employee.findOne({
+        where: { id },
+        include: [{ model: Address, as: 'addresses' }],
+      });
+    return employee;
+  }
+
 module.exports = {
     getAll,
+    getById,
 }
